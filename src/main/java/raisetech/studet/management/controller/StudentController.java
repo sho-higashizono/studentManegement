1898b27a230142de907e3d6d4b9b73115f342602
Higashizono <i.love.giants.0677@gmail.com>
2025-12-05 03:20:44 +0900

    スタンダード第14回課題

--- src/main/java/raisetech/studet/management/controller/StudentController.java
+++ src/main/java/raisetech/studet/management/controller/StudentController.java
@@ -1,6 +1,8 @@
 package raisetech.studet.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.studet.management.controller.converter.StudentConverter;
@@ -13,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class StudentController {
 
 
@@ -33,11 +35,12 @@ public class StudentController {
 
     //全件取得
     @GetMapping("/studentList")
-    public List<StudentDetail> getStudentList(){
+    public String getStudentList(Model model){
         List<Student> students = service.searchStudentList();
         List<StudentCourse> studentCourses = service.searchStudentCourse();
 
-        return converter.convertStudentDetails(students, studentCourses);
+        model.addAttribute("studentList",converter.convertStudentDetails(students,studentCourses));
+        return "studentList";
     }
 
 
